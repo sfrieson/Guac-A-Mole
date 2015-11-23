0162abf8b6313444bb9694c44175a734e7e03a69
Steven Frieson <sfrieson@gmail.com>
2015-11-23 16:27:21 -0500

    includes boardSet() and beginning of showHitPiece()

--- scripts/master.js
+++ scripts/master.js
@@ -18,25 +18,61 @@ Player
 	completion rate
 */
 
+//Data for how many pieces go on each level and how they should be styled.
+var levelData = [
+	{pieces: 6, grid: '3x2'},
+	{pieces: 9, grid: '3x3'},
+	{pieces: 12, grid: '4x3'}
+];
+
+
+//game constructor function
 var Game = function () {
-	this.level = 1;
+	this.level = 0;
 	this.activePlayer = null;
 	this.$scoreboard = $('<section id="score">');
-	this.$board = $('<section id="map">');
+	this.$map = $('<section id="map">');
+	this.$board = $('<section>').append(this.$scoreboard).append(this.$map); //board is made up of scoreboard and map sections
 };
 
 Game.prototype = {
-	setMap: function () {
-		var level = this.level;
+	//sets up the board for the beginning of a round
+	setBoard: function () {
+		var level = this.level, //get the current level
+			grid = $('<div>').attr('id', 'grid-' + levelData[level].grid), //make the grid div with size setting in ID
+			position = 0,
+			piece,
+			i;
+		
+		for (i = 0; i < levelData[level].pieces; i++) { //loops for the number of pieces for the level
+			piece = $('<div>').attr('class', 'cell-' + levelData[level].grid); //sets pieces' class
+			piece.attr('data-position', position); //set a position for each cell to for setting hit-pieces later
+			grid.append(piece); //adds piece to the grid
+			position++;
+		}
+		$('#game').append(grid); //gets grid to the game
 	},
+	
+	
 	setScoreboard: function () {
 		
 	},
-	showHit: function () {}
 	
+	//Sets a cell to be active for hit for limited time
+	showHit: function () {
+		var randCell = parseInt(Math.random() * levelData[this.level].pieces, 10);//select random cell from possible number
+		console.log(randCell);
+		randCell = $('[data-position=' + randCell + ']');
+		randCell.addClass('red-bg');
+		setTimeout(function () {
+			randCell.removeClass('red-bg');
+		}, 1000);
+	}
 };
 
 
+
+//Player constructor function
 var Player = function (name) {
 	this.name = name;
 	this.score = 0;
